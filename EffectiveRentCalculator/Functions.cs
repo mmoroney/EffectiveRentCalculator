045263828f902f6efa2f607b08c8b511c003343e
mmoroney <mikemoroney1722@hotmail.com>
2016-04-21 21:07:06 -0700

    Initial checkin for EffectiveRentCalculator. Added Sum and unit tests.

---
+++ EffectiveRentCalculator/Functions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EffectiveRentCalculator
+{
+    public static class Functions
+    {
+        public static double Sum(double a, double r, int n)
+        {
+            if (n < 0)
+                throw new ArgumentOutOfRangeException(string.Format("{0} must be greater than or equal to zero", nameof(n)));
+
+            if (r < 0.0)
+                throw new ArgumentOutOfRangeException(string.Format("{0} must be greater than or equal to zero", nameof(r)));
+
+            if (r == 1.0)
+                return a * n;
+
+            return a * (Math.Pow(r, n) - 1) / (r - 1);
+        }
+    }
+}
---
+++ EffectiveRentCalculatorTest/FunctionsTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using EffectiveRentCalculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EffectiveRentCalculatorTest
+{
+    [TestClass]
+    public class FunctionsTests
+    {
+        [TestMethod]
+        public void TestSum()
+        {
+            for(double a = 0; a < 100; a++)
+            {
+                for(double r = 0; r < 2; r += 0.1)
+                {
+                    for(int n = 0; n < 100; n++)
+                    {
+                        FunctionsTests.TestSum(a, r, n);
+                    }
+                }
+            }
+        }
+
+        private static void TestSum(double a, double r, int n)
+        {
+            double term = a;
+            double sum = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                sum += term;
+                term *= r;
+            }
+
+            double expected = Functions.Sum(a, r, n);
+
+            if (sum == 0)
+                Assert.IsTrue(expected == 0);
+            else
+                Assert.IsTrue(Math.Abs(expected / sum - 1.0) < 1e-9);
+        }
+    }
+}
