7605a87abc3a32c97b1022225b0f512f91c27797
Mike Moroney <mikemoroney1722@hotmail.com>
2016-04-23 06:23:33 -0700

    Added more functions and unit tests.

--- EffectiveRentCalculator/Functions.cs
+++ EffectiveRentCalculator/Functions.cs
@@ -8,6 +8,13 @@ namespace EffectiveRentCalculator
 {
     public static class Functions
     {
+        /// <summary>
+        /// Calculates the sum of a geometric series
+        /// </summary>
+        /// <param name="a">The first term in the series.</param>
+        /// <param name="r">The common ratio between successive terms.</param>
+        /// <param name="n">The number of terms in the series.</param>
+        /// <returns></returns>
         public static double Sum(double a, double r, int n)
         {
             if (n < 0)
@@ -20,6 +27,55 @@ namespace EffectiveRentCalculator
                 return a * n;
 
             return a * (Math.Pow(r, n) - 1) / (r - 1);
+        }
+
+        /// <summary>
+        /// Calculates the payment for a mortgage.
+        /// </summary>
+        /// <param name="p">Initial principal of the loan.</param>
+        /// <param name="i">The interest rate per period.</param>
+        /// <param name="n">The number of periods.</param>
+        /// <returns></returns>
+        public static double Payment(double p, double i, int n)
+        {
+            if (n <= 0)
+                throw new ArgumentOutOfRangeException(string.Format("{0} must be greater than zero", nameof(n)));
+
+            if (i <= 0.0)
+                throw new ArgumentOutOfRangeException(string.Format("{0} must be greater than zero", nameof(i)));
+
+            return p * i / (1 - Math.Pow(1 + i, -n));
+        }
+
+        /// <summary>
+        /// Calculates the future value of an asset.
+        /// </summary>
+        /// <param name="a">Initial value of the asset.</param>
+        /// <param name="i">The rate at which the asset increases per period.</param>
+        /// <param name="n">The number of periods.</param>
+        /// <returns></returns>
+        public static double FutureValue(double a, double i, int n)
+        {
+            if (n < 0)
+                throw new ArgumentOutOfRangeException(string.Format("{0} must be greater than or equal to zero", nameof(n)));
+
+            if (i < 0.0)
+                throw new ArgumentOutOfRangeException(string.Format("{0} must be greater than or equal to zero", nameof(i)));
+
+            return a * Math.Pow(1 + i, n);
+        }
+
+        /// <summary>
+        /// Calculates the future value of a series of increasing payments
+        /// </summary>
+        /// <param name="a">Value of the initial payment.</param>
+        /// <param name="i">Rate at which the amount of the payment increases per period.</param>
+        /// <param name="r">Rate at which the value of all money paid increases per period.</param>
+        /// <param name="n">The number of periods.</param>
+        /// <returns></returns>
+        public static double ProgressiveFutureValue(double a, double i, double r, int n)
+        {
+            return Functions.Sum(a * Math.Pow(1.0 + r, n - 1), (1.0 + i) / (1.0 + r), n);
         }
     }
 }
--- EffectiveRentCalculatorTest/FunctionsTest.cs
+++ EffectiveRentCalculatorTest/FunctionsTest.cs
@@ -16,29 +16,105 @@ namespace EffectiveRentCalculatorTest
                 {
                     for(int n = 0; n < 100; n++)
                     {
-                        FunctionsTests.TestSum(a, r, n);
+                        double term = a;
+                        double sum = 0;
+
+                        for (int i = 0; i < n; i++)
+                        {
+                            sum += term;
+                            term *= r;
+                        }
+
+                        double expected = Functions.Sum(a, r, n);
+
+                        if (sum == 0)
+                            Assert.IsTrue(expected == 0);
+                        else
+                            Assert.IsTrue(Math.Abs(expected / sum - 1.0) < 1e-9);
                     }
                 }
             }
         }
 
-        private static void TestSum(double a, double r, int n)
+        [TestMethod]
+        public void TestPayment()
         {
-            double term = a;
-            double sum = 0;
-
-            for(int i = 0; i < n; i++)
+            for(double p = 0; p < 1000000; p += 10000)
             {
-                sum += term;
-                term *= r;
+                for(double i = 0.01; i < 0.2; i += 0.01)
+                {
+                    for(int n = 1; n < 30; n++)
+                    {
+                        double balance = p;
+                        double payment = Functions.Payment(p, i, n);
+
+                        for(int j = 1; j <= n; j++)
+                        {
+                            balance *= (1.0 + i);
+                            balance -= payment;
+                        }
+
+                        Assert.IsTrue(Math.Abs(balance) < 1e-5);
+                    }
+                }
             }
+        }
 
-            double expected = Functions.Sum(a, r, n);
+        [TestMethod]
+        public void TestFutureValue()
+        {
+            for(double a = 0.0; a < 100; a++)
+            {
+                for(double i = 0.0; i < 0.2; i += 0.05)
+                {
+                    for(int n = 1; n < 50; n++)
+                    {
+                        double value = a;
+                        for(int j = 1; j <= n; j++)
+                            value *= (1.0 + i);
 
-            if (sum == 0)
-                Assert.IsTrue(expected == 0);
-            else
-                Assert.IsTrue(Math.Abs(expected / sum - 1.0) < 1e-9);
+                        double expected = Functions.FutureValue(a, i, n);
+
+                        if (value == 0)
+                            Assert.IsTrue(expected == 0);
+                        else
+                            Assert.IsTrue(Math.Abs(expected / value - 1.0) < 1e-9);
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestProgressiveFutureValue()
+        {
+            for(double a = 0; a < 100; a++)
+            {
+                for(double i = 0.0; i < 0.2; i += 0.05)
+                {
+                    for(double r = 0.0; r < 0.2; r += 0.05)
+                    {
+                        for(int n = 1; n < 50; n++)
+                        {
+                            double value = 0;
+                            double payment = a;
+
+                            for(int j = 1; j <= n; j++)
+                            {
+                                value *= (1.0 + r);
+                                value += payment;
+                                payment *= (1.0 + i);
+                            }
+
+                            double expected = Functions.ProgressiveFutureValue(a, i, r, n);
+
+                            if (value == 0)
+                                Assert.IsTrue(expected == 0);
+                            else
+                                Assert.IsTrue(Math.Abs(expected / value - 1.0) < 1e-9);
+                        }
+                    }
+                }
+            }
         }
     }
 }
